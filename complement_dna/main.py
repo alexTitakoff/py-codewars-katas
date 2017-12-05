1656b773d18293d04511b4ec47599d0aa7bdc831
Alex Titakoff <titakovad@webinnovations.ru>
2017-12-05 11:38:20 +0300

    init python katas

---
+++ complement_dna/main.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/complementary-dna/train/python
+
+test_dna = "ATTGC"
+dna = test_dna
+
+
+def DNA_strand(dna):
+    my_other_dict = {"one": 1, "two": 2, "three": 3}
+    print(my_other_dict["one"])
+
+
+
+DNA_strand()
